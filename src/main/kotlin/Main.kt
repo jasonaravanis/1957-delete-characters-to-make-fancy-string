cac902b049af0825e0c9bbcf21e49650dace009c
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2025-07-27 16:19:39 +1000

    feat: add my implementation

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun makeFancyString(s: String): String {
+    val strBuilder = StringBuilder()
+
+    for ((index, character) in s.withIndex()) {
+        if (index == 0 || index == s.length - 1) {
+            strBuilder.append(character)
+            continue
+        }
+
+        if (s[index - 1] == character && s[index + 1] == character) {
+            continue
+        } else {
+            strBuilder.append(character)
+        }
+    }
+
+    return strBuilder.toString()
+}
+
+fun main() {
+    val input = "aab"
+
+    println(makeFancyString(input))
+}
